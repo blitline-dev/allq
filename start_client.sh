c42adf944d7211d47a6d234fa8b4938bce8034e0
Jason <jaciones@gmail.com>
2018-03-28 14:41:01 -0700

    So far

--- start_client.sh
+++ start_client.sh
@@ -1,1 +1,3 @@
+if [ "x$SERVER_IP" == "x" ]; then echo "No SERVER_IP set using localhost"; fi
+if [ "x$SERVER_PORT" == "x" ]; then echo "No SERVER_PORT set using 7788"; fi
 crystal src/all_q/client/client.cr
