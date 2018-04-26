ad2b366ff37dbdcf0417252f5a2a133022b61c19
Jason <jaciones@gmail.com>
2018-04-26 16:10:52 -0700

    Updated key ENV options

--- src/all_q/client/server_connection.cr
+++ src/all_q/client/server_connection.cr
@@ -1,10 +1,10 @@
 module AllQ
   class ServerConnection
     @server_client : ZMQ::Socket
-    A_ZAP_DOMAIN      = ENV["A_ZAP_DOMAIN"]? || "roger"
-    A_CURVE_SERVERKEY = ENV["A_CURVE_SERVERKEY"]? || "W}@/*{s8T8&/j%H5>>/m+O?MdJO]$Vbo2FC0pAS@"
-    A_CURVE_PUBLICKEY = ENV["A_CURVE_PUBLICKEY"]? || "kA3:7cq}Pv+-#j9bNLZIwkWDb[0]71E@kVPl9hg}"
-    A_CURVE_SECRETKEY = ENV["A_CURVE_SECRETKEY"]? || "rimy00EMw2WO>sctSIe5rw&9c8*qz*jeg+:S.?!n"
+    A_ZAP_DOMAIN              = ENV["A_ZAP_DOMAIN"]? || "roger"
+    A_CURVE_SERVER_PUBLIC_KEY = ENV["A_CURVE_SERVERKEY"]? || "W}@/*{s8T8&/j%H5>>/m+O?MdJO]$Vbo2FC0pAS@"
+    A_CURVE_PUBLICKEY         = ENV["A_CURVE_PUBLICKEY"]? || "kA3:7cq}Pv+-#j9bNLZIwkWDb[0]71E@kVPl9hg}"
+    A_CURVE_SECRETKEY         = ENV["A_CURVE_SECRETKEY"]? || "rimy00EMw2WO>sctSIe5rw&9c8*qz*jeg+:S.?!n"
 
     property id
 
@@ -46,6 +46,7 @@ module AllQ
         string_to_send = hash.to_json
         string_to_send = string_to_send.gsub(/\"job_id\"\s*:\s*\".*?\,(.*?)\"/, "\"job_id\":\"\\1\"")
         string_to_send = string_to_send.gsub(/\"parent_id\"\s*:\s*\".*?\,(.*?)\"/, "\"parent_id\":\"\\1\"")
+
         @server_client.send_string(string_to_send)
       rescue ex
         puts "Exception1 is send_string"
@@ -93,8 +94,8 @@ module AllQ
     def start_server_connection(server_client)
       spawn do
         server_client.set_socket_option(::ZMQ::ZAP_DOMAIN, A_ZAP_DOMAIN)
-        server_client.set_socket_option(::ZMQ::CURVE_SERVER, 1)
-        server_client.set_socket_option(::ZMQ::CURVE_SERVERKEY, A_CURVE_SERVERKEY)
+        server_client.set_socket_option(::ZMQ::CURVE_SERVER, 0)
+        server_client.set_socket_option(::ZMQ::CURVE_SERVERKEY, A_CURVE_SERVER_PUBLIC_KEY)
         server_client.set_socket_option(::ZMQ::CURVE_PUBLICKEY, A_CURVE_PUBLICKEY)
         server_client.set_socket_option(::ZMQ::CURVE_SECRETKEY, A_CURVE_SECRETKEY)
 
