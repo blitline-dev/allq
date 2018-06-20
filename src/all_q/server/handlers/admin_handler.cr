598c8fea773a76a3e7580651d32d2159630ce87e
Jason <jaciones@gmail.com>
2018-06-20 13:44:39 -0700

    Updated with some error handling

--- src/all_q/server/handlers/admin_handler.cr
+++ src/all_q/server/handlers/admin_handler.cr
@@ -12,6 +12,19 @@ module AllQ
         output["action_type"] = "redirect"
         @cache_store.set_redirect_info(data["server"], data["port"])
       end
+
+      if action_type && action_type.to_s == "set_throttle"
+        output["action_type"] = "set_throttle"
+        tube_name = data["name"]
+        throttle_tps = data["tps"]
+        output["name"] = tube_name
+        output["tps"] = throttle_tps
+        tube = @cache_store.tubes.get(tube_name)
+        if tube
+          tube.set_throttle(throttle_tps.to_i)
+        end
+      end
+
       return_data["result"] = output
       return return_data
     end
