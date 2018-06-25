775b3c7383b51411d78cd840e906974290500fde
Jason <jaciones@gmail.com>
2018-06-24 17:58:28 -0700

    Updated peek to seach all queues

---
+++ src/all_q/client/handlers/client_peek_handler.cr
@@ -0,0 +1,24 @@
+module AllQ
+  class ClientPeekHandler < BaseClientHandler
+    # -------------------------------------------------
+    # Peek needs to look through all queues, not just a
+    # sampled one. It should iterator until it finds
+    # one and return in. Otherwise nothing to return
+    # -------------------------------------------------
+    def process(parsed_data)
+      result_hash = Hash(String, JSON::Any).new
+      output = "{}"
+      # -- Run through connections...
+      @server_connections.values.each do |server_client|
+        output = server_client.send_string(parsed_data)
+        server_response = JSON.parse(output)
+        # -- If we find one, return it
+        if server_response["job"]? && server_response["job"]["job_id"]?
+          return output
+        end
+      end
+      # -- ...otherwise return the last one we saw (which is empty)
+      return output
+    end
+  end
+end
