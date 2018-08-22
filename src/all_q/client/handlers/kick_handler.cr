b32f556701ba2323252aeba4c8533f0a172ac655
Jason <jaciones@gmail.com>
2018-08-22 14:15:04 -0700

    Added server connection id to kick

--- src/all_q/client/handlers/kick_handler.cr
+++ src/all_q/client/handlers/kick_handler.cr
@@ -1,5 +1,6 @@
 module AllQ
   class KickHandler < BaseClientHandler
+    JOB_ID_DIVIDER        = ","
     # -------------------------------------------------
     # Peek needs to look through all queues, not just a
     # sampled one. It should iterator until it finds
@@ -7,18 +8,14 @@ module AllQ
     # -------------------------------------------------
     def process(parsed_data)
       result_hash = Hash(String, JSON::Any)
-      output = "{}"
+      full_job_id = parsed_data["params"]["job_id"]
+      tube = parsed_data["params"]["tube"]
+      vals = full_job_id.to_s.split(JOB_ID_DIVIDER)
+      connection_id = vals[0].to_s
       # -- Run through connections...
-      @server_connections.well_connections.values.each do |server_client|
-        output = server_client.send_string(parsed_data)
-        server_response = JSON.parse(output)
-        # -- If we find one, return it
-        if server_response["job"]? && server_response["job"]["job_id"]?
-          return output
-        end
-      end
-      # -- ...otherwise return the last one we saw (which is empty)
-      return output
+      server_client = @server_connections.get(connection_id)
+      output = server_client.send_string(parsed_data)
+      return output || "{}"
     end
   end
 end
