a9a6c099cf871bf3448beede482aa6e05f29b2d8
Jason <jaciones@gmail.com>
2019-01-19 15:20:25 -0800

    Fixes

---
+++ deploy/client/cli.sh
@@ -0,0 +1,13 @@
+echo "Attempting to run CLI"
+
+echo "A_CURVE_PUBLICKEY=$A_CURVE_PUBLICKEY"
+echo "A_CURVE_SECRETKEY=$A_CURVE_SECRETKEY"
+echo "A_CURVE_SERVER_PUBLICKEY=$A_CURVE_SERVER_PUBLICKEY"
+echo "SERVER_STRING=$SERVER_STRING"
+echo "TCP_CLIENT_PORT=$TCP_CLIENT_PORT"
+echo "ALLQ_DEBUG=$ALLQ_DEBUG"
+
+cd allq_cli
+/opt/rubies/ruby-2.5.3/bin/ruby main.rb
+
+
