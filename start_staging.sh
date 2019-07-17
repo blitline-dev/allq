0b60af62b6e27a13a88f015c942b2f97eb94a2dc
Jason <jaciones@gmail.com>
2019-07-17 10:43:23 -0700

    Simple non-important update

--- start_staging.sh
+++ start_staging.sh
@@ -2,9 +2,13 @@
 export A_CURVE_PUBLICKEY=WyVIWUN5KS8yXUJyLlRCfTMqRjplMDd2IS5LQiQlaiY6ekJZVS1SawA=
 export A_CURVE_SECRETKEY=NCQvdGJ3cHl1ZihjIzlpYmdeXShIdClnOmpNSns1Ni5pJWliSmVSMAA=
 export A_CURVE_SERVER_PUBLICKEY=N21kJChibWV6TCsjLTNWdTp2Wj5INzJWdDQ1NVAoREhAYyZRIyFIagA=
-export SERVER_STRING="ec2-18-233-152-155.compute-1.amazonaws.com:7788,ec2-18-212-168-58.compute-1.amazonaws.com:7788"
+export SERVER_STRING=ec2-54-175-254-132.compute-1.amazonaws.com:7788,ec2-54-161-53-83.compute-1.amazonaws.com:7788
+export TCP_CLIENT_PORT=7768
+
 
 export ALLQ_DEBUG=true
 export USE_SWEEPER=true
 
 crystal src/all_q/client/client.cr
+
+
