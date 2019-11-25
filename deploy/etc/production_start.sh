0262b13018055da1add93db311217069418d33ea
Jason <jaciones@gmail.com>
2019-11-25 15:23:51 -0800

    Fixes for etcd

--- deploy/etc/production_start.sh
+++ deploy/etc/production_start.sh
@@ -1,8 +1,8 @@
 docker rm -f etcd_listener
 sudo docker run --name=etcd_listener \
-   -e "WATCH_PATH=/production/web/allq_servers_us" \
+   -e "WATCH_PATH='/production/web/allq_servers_us'" \
    -e "ACTION_NAME=update_servers" \
    -e "PARAM_NAME=servers" \
    -e "SOCKET_LOCATION=/tmp/allq_client.sock" \
-   etcd_listener:latest
+   etcd_listener
 
