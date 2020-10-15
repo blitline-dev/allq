42432ef3014890fa26c3806c36726e1f1bbb3aca
Jason <jaciones@gmail.com>
2020-10-15 12:30:26 -0700

    drop

--- start_client.sh
+++ start_client.sh
@@ -1,5 +1,11 @@
 if [ "x$SERVER_IP" == "x" ]; then echo "No SERVER_IP set using localhost"; fi
 if [ "x$SERVER_PORT" == "x" ]; then echo "No SERVER_PORT set using 7788"; fi
+
+export SERVER_STRING=3.88.240.120:7788,54.156.34.209:7788
+export A_CURVE_PUBLICKEY=PV4mOCFMN1YqQkg+SytCTzdheD9vPz93N3pLfVV4MzxzNF5aJnhsdwA=
+export A_CURVE_SECRETKEY=cUVzQU1xOXY6N2dsMzRQR1ApNVkodmt7Un1hJVpLQTd9QVRjJTZEKwA=
+export A_CURVE_SERVER_PUBLICKEY=bmElYUctdGZvPFVEYTZFbSElZmhVVTNlcUohTy5hMDpkdmVzcUBzIQA=
+
 export ALLQ_DEBUG=true
 export USE_SWEEPER=true
 export HTTP_SERVER_PORT=8091
