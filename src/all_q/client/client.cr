a94ffb8397f011741e3f97169ed9833c89bd9404
Jason <jaciones@gmail.com>
2021-03-09 18:42:39 -0800

    Fixes for timeouts when server hangs

--- src/all_q/client/client.cr
+++ src/all_q/client/client.cr
@@ -145,7 +145,7 @@ module AllQ
     end
 
     def progressive_backoff(hash, forced_connection, server_client)
-      1.upto(4) do |i|
+      1.upto(2) do |i|
         puts "Retrying send..."
         server_client = @server_connection_cache.restart_connection(server_client.id)
         if forced_connection
@@ -158,7 +158,7 @@ module AllQ
             return val unless val.nil?
           end
         end
-        sleep(i)
+        sleep(i * 2)
       end
       raise "Failed send to server"
     end
