87f068b5a17bf4e78f1d98681bcc75b90496f941
Jason <jaciones@gmail.com>
2021-06-18 12:55:03 -0700

    Fix for changes to .to_i for strings

--- src/all_q/server/handlers/throttle_handler.cr
+++ src/all_q/server/handlers/throttle_handler.cr
@@ -1,20 +1,19 @@
 module AllQ
-    class ThrottleHandler < BaseHandler
-      def process(json : JSON::Any)
-        data = normalize_json_hash(json)
-        tube_name = data["tube"]?
-        tps = data["tps"]?
-        handler_response = HandlerResponse.new("throttle")
+  class ThrottleHandler < BaseHandler
+    def process(json : JSON::Any)
+      data = normalize_json_hash(json)
+      tube_name = data["tube"]?
+      tps = data["tps"]?
+      handler_response = HandlerResponse.new("throttle")
 
-        if tube_name && tps
-          handler_response.value = tps.to_s
-          @cache_store.tubes[tube_name].set_throttle(tps.to_i)
-        else
-          raise "Failed to set throttle because tube name or tps is invalid"
-        end
-  
-        return handler_response
+      if tube_name && tps
+        handler_response.value = tps.to_i(strict: false).to_s
+        @cache_store.tubes[tube_name].set_throttle(tps.to_i(strict: false))
+      else
+        raise "Failed to set throttle because tube name or tps is invalid"
       end
+
+      return handler_response
     end
   end
-  
+end
