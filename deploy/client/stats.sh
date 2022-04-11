f963c2df89d7da60dab989ae4c0dcd282a3b541b
Jason <jaciones@gmail.com>
2022-04-10 21:24:41 -0700

    Fix for logging during multiprocess

--- deploy/client/stats.sh
+++ deploy/client/stats.sh
@@ -3,5 +3,5 @@
 PORT="${STATS_PORT:-7768}"
 TIMEOUT="${SERVER_TIMEOUT:-10}"
 
-echo '{"action" : "stats", "params" : {}}' | socat -t $TIMEOUT -T $TIMEOUT - tcp4-connect:localhost:$PORT
+echo '{"action" : "stats", "params" : {}}' | socat -t $TIMEOUT -T $TIMEOUT - tcp4-connect:localhost:$PORT > /dev/null
 
